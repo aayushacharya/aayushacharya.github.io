a3cfd5dffb8392c392e5635808755f1e5c855049
Aayush Acharya <ayushacharya99@gmail.com>
2021-11-08 19:42:59 +0545

    Added TagIcon and BlogTags components

--- src/components/atoms.js
+++ src/components/atoms.js
@@ -11,6 +11,54 @@ export const NavLink = ({ to, children, title = "Link", selected = false }) => {
   )
 }
 
+export const TagIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="currentColor"
+      className="bi bi-tags-fill mr-4"
+      style={{ display: "inline-block" }}
+      viewBox="0 0 16 16"
+    >
+      <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+      <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z" />
+    </svg>
+  )
+}
+
+export const BlogTags = ({ tags, children }) => {
+  return (
+    <p className="font-bold text-base text-tertiary mb-2">
+      {children}
+      {tags.map((eachTag, index) => {
+        if (index === 0) {
+          return (
+            <div
+              className="bg-secondary p-1 rounded-md"
+              style={{ display: "inline-block" }}
+            >
+              {eachTag}
+            </div>
+          )
+        }
+        return (
+          <>
+            {" • "}
+            <div
+              className="bg-secondary p-1 rounded-md"
+              style={{ display: "inline-block" }}
+            >
+              {eachTag}
+            </div>
+          </>
+        )
+      })}
+    </p>
+  )
+}
+
 export const BlogTitle = ({ children }) => {
   return (
     <h1
@@ -22,12 +70,21 @@ export const BlogTitle = ({ children }) => {
   )
 }
 
-export const BlogTitleInfo = ({ timeToRead, date, datetime, updated = false, updated_datetime }) => {
-  const update_comp=(
-  <span>
-    <span> • </span>
-    <span>Last updated on <time dateTime={updated_datetime}>{updated}</time></span>
-  </span>)
+export const BlogTitleInfo = ({
+  timeToRead,
+  date,
+  datetime,
+  updated = false,
+  updated_datetime,
+}) => {
+  const update_comp = (
+    <span>
+      <span> • </span>
+      <span>
+        Last updated on <time dateTime={updated_datetime}>{updated}</time>
+      </span>
+    </span>
+  )
 
   return (
     <div className="mb-2 text-sm text-tertiary tracking-normal">
