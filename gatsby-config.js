c3981b534e498f8514d8bb491e59b7395169d992
Aayush Acharya <ayushacharya99@gmail.com>
2023-11-29 08:58:38 +0545

    fix: blurred background image in blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,6 +85,7 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
+              disableBgImage: true
             },
           },
           {
